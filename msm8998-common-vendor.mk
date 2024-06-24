54a412199a5f114128e93064a0df341a167eddaf
Georg Veichtlbauer <georg@vware.at>
2024-06-24 15:46:30 +0200

    msm8998-common: Add IMS and EGL symlinks
    Change-Id: I536ef36ce3725be3fd34e0a10a3d703b5afd4985

--- msm8998-common-vendor.mk
+++ msm8998-common-vendor.mk
@@ -779,3 +779,13 @@ PRODUCT_PACKAGES += \
     qcrilhook \
     android.hardware.gnss@2.0-service-qti \
     vendor.qti.gnss@3.0-service
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64 \
+    libEGL_adreno_libEGL_adreno_symlink32 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink32 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink32 \
+    libEGL_adreno_libEGL_adreno_symlink64 \
+    libGLESv2_adreno_libGLESv2_adreno_symlink64 \
+    libq3dtools_adreno_libq3dtools_adreno_symlink64
